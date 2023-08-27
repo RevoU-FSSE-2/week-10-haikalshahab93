fca0efae5d0a7e514aa8bbe87a0b5db516a673c0
haikalshahab93 <haikal.pusri@gmail.com>
2023-08-27 19:53:18 +0700

    update router

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const openApiValidator = require('express-openapi-validator')
 const yaml = require('yaml')
 const swaggerUi = require('swagger-ui-express')
 
+// Router
+const authRouter = require('./routes/auth.js')
+// const transferReqRouter = require('./routes/transfer-req.js')
+
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// parses ke json 
+app.use(bodyParser.json())
+
+// user
+app.use('/v1/auth', authRouter)
 
 app.use('/',(req,res)=>{
     console.log("berhasil")
--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require("mongodb");
 
+
+const DB_URI = process.env.ENV === 'production' ? process.env.MONGO_PROD : process.env.MONGO_URI 
 const connectToDb = async () => {
   try {
-    const client = await new MongoClient(process.env.MONGO_URI).connect();
+    const client = await new MongoClient(DB_URI).connect();
     const db = client.db(process.env.MONGO_DB);
-    console.log( `<=================== CONNECT TO DB ==================`);
+    console.log( `Connection Success`);
     return db
   } catch (error) {
     console.log(error);
